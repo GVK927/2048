e26941e2692301fca267efdf3df4a2c585913ca2
927 <glebkalinin927@gmail.com>
2022-05-01 22:14:22 +0300

    Initial commit

---
+++ app/src/main/java/Game/GameMain.java
@@ -0,0 +1,12 @@
+package Game;
+
+public class GameMain {
+    private GameLogic gameBoard;
+    private MainMenu mainMenu;
+    private IngameMenu ingameMenu;
+
+    public GameMain(){
+        this.gameBoard = new GameLogic();
+
+    }
+}
